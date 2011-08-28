3761a668d8c34be4cd11af8e848eb5e963b5221f
Vernon Swanepoel <vernon@sandcurves.com>
2011-08-28 23:40:59 +0100

    Starting the brain calculations program

---
+++ brainc.c
@@ -0,0 +1,64 @@
+/* ************************************************************************
+*                                                                         *
+*   Do calculation problems to sharpen the noggin                         *
+*                                                                         *
+*   Copyright (C) 2011 by Vernon Swanepoel                                *
+*   vernon@sandcurves.com                                                 *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+*                                                                         *
+***************************************************************************/
+
+#include <stdio.h>                                  /* for io and stuff like that */
+#include <ctype.h>                                  /* for isdigit and isalnum */
+#include <stdlib.h>                                 /* for rand() */
+#include <time.h>                                   /* for seeding rand() */
+#include <unistd.h>                                 /* for sleep() */
+#define FIRST_ARG 1                                 /* First commandline argument */
+extern char *stripdash(char *arg);
+void help();
+int main(int argc, char **argv)
+{
+  /* Ask user for the number of digits to enter 1 - 3 */
+
+  /* Main as a big switch */
+    switch(stripdash(argv[FIRST_ARG])[0]){
+    case 'a':                              /* addition */
+      printf("got here\n");
+      break;
+    case 's':                              /* subtraction */
+      printf("got here\n");
+      break;
+    case 'd':                              /* division */
+      printf("got here\n");
+      break;
+    case 'm':                              /* multiplication */
+      printf("got here\n");
+      break;
+    case 'e':                              /* everything */
+      printf("got here\n");
+      break;
+    case 'h':
+      help();
+      break;
+    }
+  return(EXIT_SUCCESS);
+}
+void help()
+{
+  /* develop the help as the options develop in the program */
+  printf("Help\n");
+}
---
+++ stripdash.c
@@ -0,0 +1,7 @@
+char *stripdash(char *arg) /* if the args have dashes, strip them */
+{
+  int i;
+  for(i = 0; arg[i] == '-'; i++)
+    ;
+  return &arg[i];
+}
